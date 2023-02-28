1ad3e9a5c34d01169c450da331d4779c9cbe2d7c
unknown <azizovagultakin@gmail.com>
2023-02-28 09:35:02 +0300

    first commit

---
+++ fev-28/fev-28/Program.cs
@@ -0,0 +1,36 @@
+﻿//task 3 sehv
+
+//int base=2, power = 3;
+//for (int i = 0; i < 9; i++)
+//{
+//    if (---)
+//    { }
+//    Console.WriteLine();
+//        }}
+
+
+
+//task4
+//int num = 564;
+//int A = 5;
+//int B = 6;
+//int C = 4;
+//int larg = 0;
+//int smallest = 0;
+
+
+//if (A < B && A > C)
+//{
+//    larg = B;smallest= C;   
+//}
+//else if (B > A && B > C)
+//{
+//    larg = B; larg = B;
+//}
+//if (C < A && C < B)
+//{
+
+//    smallest = C; larg=B;
+//}
+
+//Console.WriteLine(larg);
